b38945335f20e3a9cb374769a2384c4bd4887bf6
Mason F. Matthews <mason.f.matthews@gmail.com>
2013-10-02 07:19:55 -0400

    Continue to set up login/user workflows.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def admin_only!
+    redirect_to "/", flash: { error: 'Only admins can access this page.' } unless current_user.admin
+  end
 end
--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,5 @@
 class CoursesController < ApplicationController
-  before_filter :authenticate_user!, except: :show
+  before_action :authenticate_user!, except: :show
 
   def show
     @course = Course.find(params[:id])

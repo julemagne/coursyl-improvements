86e085f8cd109299bfaed9714c5dd9ef5ea87c04
Mason F. Matthews <mason.f.matthews@gmail.com>
2013-10-09 11:13:59 -0400

    Complete devise styling and setup.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
   def admin_only!
     redirect_to "/", flash: { error: 'Only administrators can access this page.' } unless current_user.admin
+  end
+
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :title
+    devise_parameter_sanitizer.for(:sign_up) << :first_name
+    devise_parameter_sanitizer.for(:sign_up) << :last_name
   end
 end

b27a1fcc54a58cd13175cab45ea427500790169c
Mason F. Matthews <mason.f.matthews@gmail.com>
2014-02-14 12:29:26 -0500

    Continue new course creation process.  Generalize association creation javascript.

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,13 +1,34 @@
 class CoursesController < ApplicationController
   before_action :authenticate_user!, except: :show
+  before_action :set_course, only: [:show, :edit, :update, :destroy]
 
+  # GET /courses/new
   def new
     @course = Course.new
+    @course.course_instructors.build(instructor_id: current_user.id, primary: true)
   end
 
+  # GET /courses/1
   def show
-    @course = Course.find(params[:id])
   end
 
+  # POST /courses
+  def create
+    @course = Course.new(params)
+
+    respond_to do |format|
+      if @course.save
+        format.html { redirect_to @course, flash: {success: 'Course was successfully created.'} }
+      else
+        format.html { render action: 'new' }
+      end
+    end
+  end
+
+  private
+
+    def set_course
+      @course = Course.find(params[:id])
+    end
 
 end

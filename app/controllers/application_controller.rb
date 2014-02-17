468bfc569133339efc3111a0487200c5cb8c2ee1
Mason F. Matthews <mason.f.matthews@gmail.com>
2014-02-17 15:04:20 -0500

    Add middle name field, fix caption for ends_on.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,6 +20,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :title
     devise_parameter_sanitizer.for(:sign_up) << :first_name
+    devise_parameter_sanitizer.for(:sign_up) << :middle_name
     devise_parameter_sanitizer.for(:sign_up) << :last_name
     devise_parameter_sanitizer.for(:sign_up) << :phone
     devise_parameter_sanitizer.for(:sign_up) << :wants_to_be_instructor

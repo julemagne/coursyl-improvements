c49599659d0af20edab8a81d949981465e5cc246
Julie David <julie.angela.david@gmail.com>
2015-10-13 01:51:57 -0400

    Save button becomes disabled after it is clicked once. Refactor show and hide row.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,26 +15,58 @@
 //= require d3
 //= require_tree .
 
-function disableClick(){
-  ;
+function disableClick() {
+  document.getElementsByName("commit")[0].disabled = true;
+}
+
+function superTimeout() {
+  window.setTimeout(disableClick, 500) //Not putting a semicolon made it work WHYYYYYYY
 }
 
 function hideRow() {
-    var rows = document.getElementsByClassName("association container");
-    var lastRow = rows[rows.length-1];
-    lastRow.style.display = "none";
+  document.getElementsByClassName("associations")[0].lastElementChild.style.display = 'none';
 }
 
 function showRow() {
-  var rows = document.getElementsByClassName("association container");
-  var lastRow = rows[rows.length-1];
-  lastRow.style.display = "block";
+  document.getElementsByClassName("associations")[0].lastElementChild.style.display = 'block';
 }
 
-function deleteButton(){
-  document.getElementsByClassName=("btn");
+function deleteButton() {
+  document.getElementsByClassName("association container").style.display = "none";
 }
 
-function smoothScrolling(){
-  ;
-}
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  "use strict"
+
+  var links = document.querySelectorAll("a.scroll")
+  var i = links.length
+  var root = /firefox|trident/i.test(navigator.userAgent) ? document.documentElement : document.body
+  var easeInOutCubic = function(t, b, c, d) {
+    if ((t/=d/2) < 1) return c/2*t*t*t + b
+    return c/2*((t-=2)*t*t + 2) + b
+  }
+
+  while (i--)
+    links.item(i).addEventListener("click", function(e) {
+      var startTime
+      var startPos = root.scrollTop
+      var endPos = document.getElementById(/[^#]+$/.exec(this.href)[0]).getBoundingClientRect().top
+      var maxScroll = root.scrollHeight - window.innerHeight
+      var scrollEndValue = startPos + endPos < maxScroll ? endPos : maxScroll - startPos
+      var duration = 900
+      var scroll = function(timestamp) {
+        startTime = startTime || timestamp
+        var elapsed = timestamp - startTime
+        var progress = easeInOutCubic(elapsed, startPos, scrollEndValue, duration)
+        root.scrollTop = progress
+        elapsed < duration && requestAnimationFrame(scroll)
+      }
+      requestAnimationFrame(scroll)
+      e.preventDefault()
+    })
+})
